352e11845cacefc75b13f6dcadb5339f6bc102af
Parbat Lama <lamaparbat70@gmail.com>
2022-11-15 22:23:35 +0545

    Conversion constants to hashmap dictioneries ds

--- Constants.sol
+++ Constants.sol
@@ -3,6 +3,12 @@
 pragma solidity ^0.8.0;
 
 contract Constants {
+    string[] public DistrictLevelPosition = [
+        "Mayor",
+        "Deputy Mayor",
+        "Ward Chairperson"
+    ];
+
     string[] public Parties = [
         "NEPALI CONGRESS",
         "EMALAY",
@@ -20,12 +26,6 @@ contract Constants {
         "Local Election"
     ];
 
-    string[] public DistrictLevelPosition = [
-        "Mayor",
-        "Deputy Mayor",
-        "Ward Chairperson"
-    ];
-
     string[] public Provinces = [
         "Eastern Province",
         "Madhesh Pradesh",
@@ -35,4 +35,46 @@ contract Constants {
         "Karnali Pradesh",
         "Sudur Pashchim Pradesh"
     ];
+
+    mapping(string => uint256) public DistrictLevelPositionDict;
+    mapping(string => uint256) public PartiesDict;
+    mapping(string => uint256) public ElectionTypeDict;
+    mapping(string => uint256) public ProvincesDict;
+
+    constructor(){
+        uint256 counter = 1;
+        
+        // init DistrictLevelPosition dictionery
+        for(uint i=0;i<DistrictLevelPosition.length;i++){
+            DistrictLevelPositionDict[DistrictLevelPosition[i]] = 1100+counter;
+            counter++;
+        }
+
+        // reset couter value
+        counter = 1;
+
+        // init parties dictionery
+        for(uint i=0;i<Parties.length;i++){
+            PartiesDict[Parties[i]] = 2200+counter;
+            counter++;
+        }
+
+        // reset couter value
+        counter = 1;
+
+        // init ElectionType dictionery
+        for(uint i=0;i<ElectionType.length;i++){
+            ElectionTypeDict[ElectionType[i]] = 3300+counter;
+            counter++;
+        }
+
+        // reset couter value
+        counter = 1;
+
+        // init Provinces dictionery
+        for(uint i=0;i<Provinces.length;i++){
+            ProvincesDict[Provinces[i]] = 4400+counter;
+            counter++;
+        }
+    }
 }
