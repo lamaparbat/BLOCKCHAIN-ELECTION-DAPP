3c1913fc3f9643289a12e82a6cd3b2abfc893e9a
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2022-12-05 22:52:51 +0545

    architecture updated

--- backend/server.ts
+++ backend/server.ts
@@ -1,2 +1,22 @@
 const cluster = require("cluster");
-const app = require("./app");
+const totalCpus = require("os").cpus().length;
+
+if (cluster.isMaster) {
+ console.log(`Master node:${process.pid} is running.`);
+
+ // fork worker nodes
+ for (let i = 1; i <= totalCpus; i++) {
+  cluster.fork();
+ }
+
+ // listenting to the worker node deaths
+ cluster.on("exit", (worker: any, code: any, signal: any) => {
+  console.log(`Worker node:${worker.process.pid} died.`);
+
+  // fork another worker node
+  cluster.fork();
+ });
+
+} else {
+ require("./app");
+}
