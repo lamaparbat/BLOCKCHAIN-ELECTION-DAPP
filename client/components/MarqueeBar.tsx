dff106bab7c05a12356ea4af7b1fefd66583fd05
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2023-01-15 20:27:02 +0530

    added marquee tag popup on timeout

--- client/components/MarqueeBar.tsx
+++ client/components/MarqueeBar.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import Marquee from "react-fast-marquee";
+import { useDispatch } from 'react-redux';
 import { getEventDate } from '../utils/getEventDate';
+import { setElectionTimeCounter } from '../redux/electionTimeCounter';
 
 const MarqueeBar = ({ counterData }): React.ReactElement => {
   const [counter, setCounter] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
   const [diffInMillis, setDiffInMillis] = useState(1);
+  const dispatch = useDispatch();
 
   if (counterData.startDate) {
     const interval = setInterval(() => {
@@ -12,13 +15,15 @@ const MarqueeBar = ({ counterData }): React.ReactElement => {
       setDiffInMillis(diffInMillis);
       setCounter({ days, hours, minutes, seconds });
     }, 1000);
-    console.log(diffInMillis)
-    if (diffInMillis <= 0) clearInterval(interval);
+    if (diffInMillis <= 0) {
+      clearInterval(interval);
+      dispatch(setElectionTimeCounter({startDate:"", endDate:""}));
+    }
   }
 
   return (
     <Marquee className='h-[30px] bg-red-500 text-sm text-light'>
-      {counterData.title}
+      {counterData.title} &nbsp;
       Election starts on: Days:{counter.days} Hours:{counter.hours} Minutes:{counter.minutes} Seconds:{counter.seconds}
     </Marquee>
   )
