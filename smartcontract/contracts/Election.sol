4fa5cf99124c86134058cc18dadf58e984e5abd0
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2023-02-02 23:49:53 +0545

    update Candidate and Voter schema structures

--- smartcontract/contracts/Election.sol
+++ smartcontract/contracts/Election.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "./Structure.sol";
 
-contract Election is Structure {
-    using SafeMath for uint256;
-
-    // Mapping
-    mapping(string => Candidate) public candidates;
-    mapping(string => Voter) public voters;
+contract Election is Structure{
+    using SafeMath for uint;
+    
+    // Mapping 
+    mapping (string => Candidate) public candidates;
+    mapping (string => Voter) public voters;
 
     // Arrays
     Candidate[] public candidateList;
@@ -17,10 +17,12 @@ contract Election is Structure {
     string[] public candidateNames;
     string[] public voterNames;
 
+
+
     // static variables
-    uint256 public totalCandidate = 0;
-    uint256 public totalVoter = 0;
-    uint256 public voteCount;
+    uint public totalCandidate = 0;
+    uint public totalVoter = 0;
+    uint public voteCount;
 
     // Event
     event VoteCast(string candidateName);
@@ -28,39 +30,30 @@ contract Election is Structure {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         return a + b;
     }
-
+    
     // updators
     function vote(string memory candidateName) public {
-        candidates[candidateName].voteCount = candidates[candidateName]
-            .voteCount
-            .add(1);
+        candidates[candidateName].voteCount = candidates[candidateName].voteCount.add(1);
         voteCount = voteCount.add(1);
 
         emit VoteCast(candidateName);
     }
 
+    
+
     // setter functions
-    function addCandidate(
-        string memory _name,
-        uint256 _citizenshipNo,
-        uint256 _age,
-        string memory _agenda,
-        string memory _dob,
-        string memory _address,
-        string memory _email,
-        string memory _profile,
-        string memory _partyName
-    ) public payable {
+    function addCandidate(string memory _name, uint _citizenshipNo, uint _age, string memory _agenda, string memory _dob,
+        string memory _address,  string memory _email, string memory _profile, string memory _partyName) public payable {
         Candidate memory candidate = Candidate({
-            name: _name,
-            citizenshipNo: _citizenshipNo,
-            age: _age,
-            agenda: _agenda,
-            dob: _dob,
-            address_name: _address,
-            email: _email,
-            profile: _profile,
-            partyName: _partyName,
+            fullName: _name,
+            citizenshipNumber:_citizenshipNo,
+            age:_age,
+            agenda:_agenda,
+            dob:_dob,
+            address_name:_address,
+            email:_email,
+            profile:_profile,
+            partyName:_partyName,
             voteCount: 0
         });
 
@@ -70,57 +63,47 @@ contract Election is Structure {
         totalVoter = totalVoter.add(1);
     }
 
-    function addVoter(
-        string memory _name,
-        uint256 _citizenshipNo,
-        uint256 _age,
-        string memory _dob,
-        string memory _address,
-        string memory _email,
-        string memory _profile
-    ) public payable {
+    function addVoter(string memory _name, uint _citizenshipNo, uint _age, string memory _dob,
+        string memory _address,  string memory _email, string memory _profile) public payable {
         Voter memory voter = Voter({
-            name: _name,
-            citizenshipNo: _citizenshipNo,
-            age: _age,
-            dob: _dob,
-            address_name: _address,
-            email: _email,
-            profile: _profile,
+            fullName: _name,
+            citizenshipNumber:_citizenshipNo,
+            age:_age,
+            dob:_dob,
+            address_name:_address,
+            email:_email,
+            profile:_profile,
             voted: false
         });
-
+        
         voters[_name] = voter;
         voterNames.push(_name);
         voterList.push(voter);
         totalVoter = totalVoter.add(1);
     }
 
+
+
     // getter functions
-    function getAllCandidates() public view returns (Candidate[] memory) {
+    function getAllCandidates() public view returns (Candidate[] memory){
         return candidateList;
     }
 
-    function getAllVoters() public view returns (Voter[] memory) {
+    function getAllVoters() public view returns (Voter[] memory){
         return voterList;
     }
 
-    function getCandidateDetails(string memory name)
-        public
-        view
-        returns (Candidate memory)
-    {
+    function getCandidateDetails(string memory name) public view returns(Candidate memory){
         return candidates[name];
     }
 
-    function getVoterDetails(string memory name)
-        public
-        view
-        returns (Voter memory)
-    {
+    function getVoterDetails(string memory name) public view returns(Voter memory){
         return voters[name];
     }
 }
+
+
+
 
 // test data
 // Parbat, 0778, 22, wanna make secure system, sep 12 2022, kathamandu nepal, parbat@gmail.com, http://profile.com, Congress
--- smartcontract/contracts/Structure.sol
+++ smartcontract/contracts/Structure.sol
@@ -4,22 +4,21 @@ pragma solidity ^0.8.0;
 
 contract Structure {
     struct Candidate {
-        string name;
-        uint256 citizenshipNo;
-        uint256 age;
+        string fullName;
+        uint citizenshipNumber;
+        uint age;
         string agenda;
         string dob;
         string address_name;
         string email;
         string profile;
         string partyName;
-        uint256 voteCount;
+        uint voteCount;
     }
     struct Voter {
-        string name;
-        uint256 citizenshipNo;
-        uint256 age;
-        string agenda;
+        string fullName;
+        uint citizenshipNumber;
+        uint age;
         string dob;
         string address_name;
         string email;
