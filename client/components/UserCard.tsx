0df8db8a3e1957ac14e73b66a2830c937e94c19a
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2023-02-04 12:59:53 +0545

    Desgined Privacy Page + Election Offices Page + Resolved realtime append of FAQ question in FAQ page

--- client/components/UserCard.tsx
+++ client/components/UserCard.tsx
@@ -4,29 +4,29 @@ import Avatar from './Avatar';
 import { UserCardStruct } from '../interfaces';
 
 const UserCard: React.FC<UserCardStruct> = (props): React.ReactElement => {
- const {
-  details: { citizenshipNumber, fullName, dob, profile, education, address, contact, email },
-  type
- } = props;
+  const {
+    details: { citizenshipNo, fullName, dob, profile, education, address, contact, email },
+    type
+  } = props;
 
- return (
-  <div className='user__card flex justify-around items-center h-[170px] w-[340px] px-3 mb-3  max-[500px]:w-[500px] max-[400px]:w-full bg-slate-100 rounded-[12px] cursor-pointer hover:drop-shadow-md hover:bg-red-200 transition ease-in-out'>
-   <div className='col1 flex-col justify'>
-    <Avatar src={profile} className={''} alt={'img'} size={'xl'} border={0} />
-    <div className='social__media flex justify-center mt-2'>
-     <BsFacebook className='cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
-     <BsInstagram className='mx-4 cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
-     <BsTwitter className='cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
+  return (
+    <div className='user__card flex justify-around items-center h-[170px] w-[340px] px-3 mb-3  max-[500px]:w-[500px] max-[400px]:w-full bg-slate-100 rounded-[12px] cursor-pointer hover:drop-shadow-md hover:bg-red-200 transition ease-in-out'>
+      <div className='col1 flex-col justify'>
+        <Avatar src={profile} className={''} alt={'img'} size={'xl'} border={0} />
+        <div className='social__media flex justify-center mt-2'>
+          <BsFacebook className='cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
+          <BsInstagram className='mx-4 cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
+          <BsTwitter className='cursor-pointer hover:text-md hover:text-red-500 hover:animate-bounce' />
+        </div>
+      </div>
+      <div className='col2 h-fit flex-xl-column text-[15px] ml-2'>
+        <div>Name: {fullName}</div>
+        <div>Citizenship No: {citizenshipNo}</div>
+        <div>Date of birth: {dob}</div>
+        <div>Email: {email}</div>
+      </div>
     </div>
-   </div>
-   <div className='col2 h-fit flex-xl-column text-[15px] ml-2'>
-    <div>Name: {fullName}</div>
-    <div>Citizenship No: {citizenshipNumber}</div>
-    <div>Date of birth: {dob}</div>
-    <div>Email: {email}</div>
-   </div>
-  </div>
- )
+  )
 }
 
 export default UserCard;
