9837a94a25503dd6ba98b53af895b5384ef0c558
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2023-02-05 23:08:44 +0545

    update api response of voter, candidate and party

--- backend/src/presentation/controllers/party/index.ts
+++ backend/src/presentation/controllers/party/index.ts
@@ -7,99 +7,101 @@ const { candidateModal, partyModel } = require("../../../infrastructure/models/i
 
 
 const candidateSignup = async (req: Request, res: Response) => {
- try {
-  const { fullName, citizenshipNumber, province, district, municipality, ward, email, password } = req.body;
-  const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "candidates");
-  const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
+  try {
+    const { fullName, citizenshipNumber, province, district, municipality, ward, email, password } = req.body;
+    const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "candidates");
+    const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
 
-  await new candidateModal({
-   fullName, citizenshipNumber,
-   province, district, municipality,
-   ward, email, password, profile: imgHostedURL,
-   createdAt: Date.now()
-  }).save();
+    const response = await new candidateModal({
+      fullName, citizenshipNumber,
+      province, district, municipality,
+      ward, email, password, profile: imgHostedURL,
+      createdAt: Date.now()
+    }).save();
 
-  res.status(200).send({
-   message: "Candidate registered successfully",
-   statusCode: 200
-  });
+    res.status(200).send({
+      message: "Candidate registered successfully",
+      statusCode: 200,
+      data: response
+    });
 
- } catch (error) {
-  console.error(error)
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 };
 
 const getCandidateLists = async (req: Request, res: Response) => {
- try {
-  const { skip } = req.query;
+  try {
+    const { skip } = req.query;
 
-  const response = await candidateModal.find().skip(skip).limit(10);
-  res.status(200).send({
-   message: "Data fetched successfully",
-   data: response,
-   statusCode: 200
-  });
- } catch (error) {
-  console.error(error);
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+    const response = await candidateModal.find().skip(skip).limit(10);
+    res.status(200).send({
+      message: "Data fetched successfully",
+      data: response,
+      statusCode: 200
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 }
 
 const partySignup = async (req: Request, res: Response) => {
- try {
-  const { partyName, totalMembers, agenda } = req.body;
-  const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "party");
-  const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
+  try {
+    const { partyName, totalMembers, agenda } = req.body;
+    const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "party");
+    const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
 
-  await new partyModel({
-   partyName, totalMembers, agenda, logo: imgHostedURL,
-   createdAt: Date.now()
-  }).save();
+    const response = await new partyModel({
+      partyName, totalMembers, agenda, logo: imgHostedURL,
+      createdAt: Date.now()
+    }).save();
 
-  res.status(200).send({
-   message: "party registered successfully",
-   statusCode: 200
-  });
+    res.status(200).send({
+      message: "party registered successfully",
+      statusCode: 200,
+      data: response
+    });
 
- } catch (error) {
-  console.error(error)
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 };
 
 const getPartyLists = async (req: Request, res: Response) => {
- try {
-  const { skip } = req.query;
+  try {
+    const { skip } = req.query;
 
-  const response = await partyModel.find().skip(skip).limit(10);
-  res.status(200).send({
-   message: "Data fetched successfully",
-   data: response,
-   statusCode: 200
-  });
- } catch (error) {
-  console.error(error);
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+    const response = await partyModel.find().skip(skip).limit(10);
+    res.status(200).send({
+      message: "Data fetched successfully",
+      data: response,
+      statusCode: 200
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 }
 
 
 module.exports = {
- getCandidateLists,
- candidateSignup,
- getPartyLists,
- partySignup
+  getCandidateLists,
+  candidateSignup,
+  getPartyLists,
+  partySignup
 }
--- backend/src/presentation/controllers/voters/index.ts
+++ backend/src/presentation/controllers/voters/index.ts
@@ -6,53 +6,54 @@ const { uploadFileToFirebaseStorage } = require("../../../domain/services/index"
 const { voterModel } = require("../../../infrastructure/models/index.model");
 
 const voterSignup = async (req: Request, res: Response) => {
- try {
-  const { fullName, citizenshipNumber, province, district, municipality, ward, email, password } = req.body;
-  const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "voters");
-  const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
+  try {
+    const { fullName, citizenshipNumber, province, district, municipality, ward, email, password } = req.body;
+    const firebaseStorageResponse = await uploadFileToFirebaseStorage(UPLOAD_FOLDER_PATH, req?.file?.filename, "voters");
+    const imgHostedURL = firebaseStorageResponse[0]?.metadata?.mediaLink;
 
-  await new voterModel({
-   fullName, citizenshipNumber,
-   province, district, municipality,
-   ward, email, password, profile: imgHostedURL,
-   createdAt: Date.now()
-  }).save();
+    const result = await new voterModel({
+      fullName, citizenshipNumber,
+      province, district, municipality,
+      ward, email, password, profile: imgHostedURL,
+      createdAt: Date.now()
+    }).save();
 
-  res.status(200).send({
-   message: "Voter registered successfully",
-   statusCode: 200
-  });
+    res.status(200).send({
+      message: "Voter registered successfully",
+      statusCode: 200,
+      data: result
+    });
 
- } catch (error) {
-  console.error(error)
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 };
 
 const getVoterLists = async (req: Request, res: Response) => {
- try {
-  const { skip } = req.query;
+  try {
+    const { skip } = req.query;
 
-  const response = await voterModel.find().skip(skip).limit(10);
-  res.status(200).send({
-   message: "Data fetched successfully",
-   data: response,
-   statusCode: 200
-  });
- } catch (error) {
-  console.error(error);
-  res.status(500).send({
-   message: "Internal Server Error.",
-   statusCode: 500
-  });
- }
+    const response = await voterModel.find().skip(skip).limit(10);
+    res.status(200).send({
+      message: "Data fetched successfully",
+      data: response,
+      statusCode: 200
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      message: "Internal Server Error.",
+      statusCode: 500
+    });
+  }
 }
 
 
 module.exports = {
- voterSignup,
- getVoterLists,
+  voterSignup,
+  getVoterLists,
 }
