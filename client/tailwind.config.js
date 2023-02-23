3d0a2f4787b17a3fcf94b44dc4513343d263aeae
Parbat Lama <lamaparbat70@gmail.com>
2023-02-23 23:08:38 +0545

    customize custom viewport class for responsive view in tailwind config files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+      "sm":"300px",
+      "md":"768px",
+      "lg":"1100px"
+    },
     extend: {
       backgroundImage: {
         'celebrationGif': "url('/images/giphy.gif')"
