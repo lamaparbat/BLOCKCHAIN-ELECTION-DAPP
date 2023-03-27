81c81ce9f92fda0c4e83f116a77d38e9e77a07f3
Parbat Lama <lamaparbat70@gmail.com>
2023-03-27 22:09:25 +0545

    limit the search result chunk to 6

--- client/components/SearchModal.tsx
+++ client/components/SearchModal.tsx
@@ -1,13 +1,17 @@
+import _ from 'lodash';
+import { useRouter } from 'next/router';
 import React, { useState, useEffect, useRef } from 'react';
 import { Modal } from 'react-bootstrap';
 import { AiOutlineSearch } from 'react-icons/ai';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { GLOBAL_SEARCH_KEYWORD } from '../constants';
 
 const SearchModal = ({ show, setOpenSearchModal }) => {
   const [loading, setLoading] = useState(false);
   const [searchResult, setSearchResult] = useState([]);
   const modalRef = useRef(null);
+  const router = useRouter();
 
   // on didmount
   useEffect(() => {
@@ -20,11 +24,27 @@ const SearchModal = ({ show, setOpenSearchModal }) => {
     }
   }, []);
 
-  const onChange = (e) => {
+  useEffect(() => {
+    const e = {target: {value:"a"}};
+    if(show) onChange(e)
+  }, [show])
 
+  const onChange = (e:any) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      const keyword = e?.target?.value?.toLocaleLowerCase();
+      const filterArray = _.filter(GLOBAL_SEARCH_KEYWORD, (result) => { return _.includes(_.toLower(result.keywords.join()), keyword)})
+
+      setSearchResult(filterArray?.slice(0,6))  
+    },1000)
   }
 
   const onTyping = (e) => setLoading(e.target.value.length > 0 ?? false);
+
+  const navigate = (url:string) => {
+    router.push(url);
+  }
 
   return (
     <Modal show={show} ref={modalRef}>
@@ -40,6 +60,7 @@ const SearchModal = ({ show, setOpenSearchModal }) => {
           <AiOutlineSearch className='text-xl mr-5' />
         </div>
         <hr />
+        {!loading && searchResult?.length > 0 && <h5>Search Result: {searchResult?.length}</h5>}
         {loading ?
           <SkeletonTheme baseColor="#f8f9fa" highlightColor="#e5eaef" >
             <Skeleton count={1} height={25} width={120} /><br />
@@ -49,8 +70,22 @@ const SearchModal = ({ show, setOpenSearchModal }) => {
             <Skeleton count={1} height={30} width={350} />
           </SkeletonTheme>
           : !searchResult?.length && <span className='text-secondary'>No Result Found !</span>}
-
-        {searchResult?.length > 0 && <div></div>}
+        {!loading && searchResult && searchResult?.map((d:any, i:number) => {
+          return(
+            <div className='my-4' key={i}>
+              <div>
+              <div className='py-1 px-2 bg-slate-100 font-bold text-lg cursor-pointer' onClick={() => navigate(d?.link)} key={i}>{d?.keywords[0]}</div>
+              {
+                d.keywords.map((keyword:string, i:number) => {
+                  return(
+                    <div className='ml-4 mt-1 mb-2 cursor-pointer hover:text-blue-500' onClick={() => navigate(d?.link)} key={i}>{d?.keywords[i+1]}</div>
+                  )
+                })
+              }
+              </div>
+            </div>
+          )
+        })}
       </Modal.Body>
     </Modal>
   )
