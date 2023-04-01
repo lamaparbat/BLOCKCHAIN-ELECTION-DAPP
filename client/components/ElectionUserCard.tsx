c579f714b13df83786b357a0bd35ef3d144616bd
Parbat Lama <parbatlama@Parbats-MacBook-Pro.local>
2023-04-01 19:01:22 +0545

    navbar and homepage responsiveness maintained

--- client/components/ElectionUserCard.tsx
+++ client/components/ElectionUserCard.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ElectionUserCard = ({ label, value, Icon }) => {
   return (
-    <div className='flex min-sm:px-3 min-sm:pr-5 py-1 mb-2 xsm:px-2 bg-slate-100 min-sm:w-[250px] xsm:w-[160px] overflow-hidden items-center rounded-1'>
-      <div className='min-sm:p-2 xsm:p-0 border-4 border-blue-900 rounded-circle'>{Icon}</div>
-      <div className='min-sm:mx-3 xsm:ml-2 flex flex-column'>
-        <span className='min-sm:text-[18px] xsm:text-[13px] text-blue-900 relative top-3'>{label}</span>
-        <span className='min-sm:text-[45px] xsm:text-[27px] text-red-700 mt-1'>{value}</span>
+    <div className='bg-slate-100 sm:w-[240px] xsm:w-[170px] flex sm:px-3 py-1 mb-2 xsm:px-2 overflow-hidden items-center rounded-1'>
+      <div className='sm:p-2 xsm:p-0 border-4 border-blue-900 rounded-circle'>{Icon}</div>
+      <div className='sm:ml-4 xsm:ml-2 flex flex-column'>
+        <span className='sm:text-[18px] xsm:text-[13px] text-blue-900 relative top-3'>{label}</span>
+        <span className='sm:text-[45px] xsm:text-[27px] text-red-700 mt-1'>{value}</span>
       </div>
     </div>
   )
