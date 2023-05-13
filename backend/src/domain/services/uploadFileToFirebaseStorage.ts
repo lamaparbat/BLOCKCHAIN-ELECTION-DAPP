9cd520f5972aff1f4d4690d1403b569d165aaa98
Parbat Lama <lamaparbat70@gmail.com>
2023-05-13 11:51:04 +0545

    Update uploadFileToFirebaseStorage.ts

--- backend/src/domain/services/uploadFileToFirebaseStorage.ts
+++ backend/src/domain/services/uploadFileToFirebaseStorage.ts
@@ -1,14 +1,26 @@
 const { firebaseStorageRef } = require("../../infrastructure/index");
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 module.exports = async (path: string, filename: string, storageName: string): Promise<Object> => {
-  console.log({ path, filename, storageName })
   try {
-    return await firebaseStorageRef.upload(path + filename, {
+    const isUploaded = await firebaseStorageRef.upload(path + filename, {
       public: true,
       destination: `${storageName}/${filename}`,
       metadata: { firebaseStorageDownloadTokens: uuidv4() }
     });
+
+    if (isUploaded) {
+      const folderPath = path?.substr(0, path?.length - 1);
+
+      // unsync folder
+      fs.rmdirSync(folderPath, { recursive: true });
+
+      // recreate the folder
+      fs.mkdirSync(folderPath);
+    }
+
+    return isUploaded;
   } catch (error) {
     console.error(error);
     return { message: "upload failed !", error };
